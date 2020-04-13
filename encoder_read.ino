79aa34002858c159873ecb6e7a8339b00f536cdf
Plaban Mohapatra <42575061+Mplaban@users.noreply.github.com>
2020-04-13 14:00:23 +0530

    Add files via upload

---
+++ encoder_read.ino
@@ -0,0 +1,83 @@
+
+void Count_X ()
+{
+  int A1state = digitalRead(enc1pinA);
+  int B1state = digitalRead(enc1pinB);
+  if (B1state == A1state)
+    x_count--;
+  else
+    x_count++;
+}
+void Count_Y () {
+  int A2state = digitalRead(enc2pinA);
+  int B2state = digitalRead(enc2pinB);
+  if (B2state == A2state)
+    y_count--;
+  else
+    y_count++;
+}
+void Path_process () {
+  if (x_count - lastx_count == val) {
+    last_x = x_cord;
+    x_cord++;
+    lastx_count = x_count;
+  }
+  else if (x_count - lastx_count == -val) {
+    last_x = x_cord;
+    x_cord--;
+    lastx_count = x_count;
+  }
+  if (y_count - lasty_count == val) {
+    last_y = y_cord;
+    y_cord++;
+    lasty_count = y_count;
+  }
+
+  else if (y_count - lasty_count == -val) {
+    last_y = y_cord;
+    y_cord--;
+    lasty_count = y_count;
+  }
+
+}
+void Fill_matrix() {
+
+  if ((x_cord != last_x && y_cord == last_y) || (x_cord == last_x && y_cord != last_y) || (x_cord != last_x && y_cord != last_y)) {
+    P[k][0] = x_cord;
+    P[k][1] = y_cord;
+    if ( ((check_x != P[k][0]) && (check_y != P[k][1])) || ((check_x != P[k][0]) && (check_y == P[k][1])) || ((check_x == P[k][0]) && (check_y != P[k][1]))) {
+      Serial.print("x-");
+      //Serial.print('\t');
+      Serial.print(P[k][0]);
+      Serial.print('\t');
+      Serial.print("y-");
+      Serial.print(P[k][1]);
+      Serial.println();
+      flag = k;
+      k++;
+    }
+    if(PS4.getButtonClick(TRIANGLE))
+    Map[abs(y_cord)][abs(x_cord)] = 1;
+    else {
+    if (Map[abs(y_cord)][abs(x_cord)] == 7) {
+    Map[abs(y_cord)][abs(x_cord)]=0;
+    }
+    check_x = P[flag][0]; 
+    check_y = P[flag][1];
+  }
+
+}
+}
+
+void show_matrix () {
+  Serial.println();
+  Serial.print("path-map:");
+  Serial.println();
+  for ( int i = 0; i < 7; i++) {
+    for ( int j = 0; j < 9; j++) {
+      Serial.print(Map[i][j]);
+      Serial.print('\t');
+    }
+    Serial.println();
+  }
+}
---
+++ trace_path.ino
@@ -0,0 +1,86 @@
+void path()
+{
+  cont=true;
+  if (cx_cord == P[i][0] && cy_cord != P[i][1])
+  {
+    Serial.print("FD");
+    x_count = 0;
+    y_count = 0;
+    
+    m = P[i][1] - cy_cord;
+    while (0 =< y_count && y_count < m * 1690)
+    {
+      Serial.print("M=\t");Serial.print(m);
+      Serial.print("ycount= \t");
+      Serial.println(y_count);
+      s1 =  sf * (-0.35 * 0 + 0.35 * 150 + 0.25 * 0);
+      s2 =  sf * (-0.35 * 0 + -0.35 * 150 + 0.25 * 0);
+      s3 =  sf * (0.35 * 0 + -0.35 * 150 + 0.25 * 0);
+      s4 =  sf * (0.35 * 0 + 0.35 * 150 + 0.25 * 0);
+
+      dir1 = s1 >= 0 ? 0 : 1;
+      dir2 = s2 >= 0 ? 0 : 1;
+      dir3 = s3 >= 0 ? 1 : 0;
+      dir4 = s4 >= 0 ? 0 : 1;
+
+      digitalWrite(d1, dir1);
+      digitalWrite(d2, dir2);
+      digitalWrite(d3, dir3);
+      digitalWrite(d4, dir4);
+
+      analogWrite(m1, abs(s1));
+      analogWrite(m2, abs(s2));
+      analogWrite(m3, abs(s3));
+      analogWrite(m4, abs(s4));
+    }
+
+    analogWrite(m1, 0);
+    analogWrite(m2, 0);
+    analogWrite(m3, 0);
+    analogWrite(m4, 0);
+    cy_cord = P[i][1];
+    m = 0;
+    i++;
+  }
+  else if (cx_cord != P[i][0] && cy_cord == P[i][1])
+  {
+    x_count = 0;
+    y_count = 0;
+    m = P[i][0] - cx_cord;
+    while (0 =< x_count && x_count< m * 1690)
+    {
+      Serial.print("M=\t");Serial.print(m);
+      Serial.print(" xcount= \t");
+      Serial.println(x_count);
+      s1 =  sf * (-0.35 * 150 + 0.35 * 0 + 0.25 * 0);
+      s2 =  sf * (-0.35 * 150 + -0.35 * 0 + 0.25 * 0);
+      s3 =  sf * (0.35 * 150 + -0.35 * 0 + 0.25 * 0);
+      s4 =  sf * (0.35 * 150 + 0.35 * 0 + 0.25 * 0);
+
+
+      dir1 = s1 >= 0 ? 0 : 1;
+      dir2 = s2 >= 0 ? 0 : 1;
+      dir3 = s3 >= 0 ? 1 : 0;
+      dir4 =  s4 >= 0 ? 0 : 1;
+
+      digitalWrite(d1, dir1);
+      digitalWrite(d2, dir2);
+      digitalWrite(d3, dir3);
+      digitalWrite(d4, dir4);
+
+      analogWrite(m1, abs(s1));
+      analogWrite(m2, abs(s2));
+      analogWrite(m3, abs(s3));
+      analogWrite(m4, abs(s4));
+
+    }
+    analogWrite(m1, 0);
+    analogWrite(m2, 0);
+    analogWrite(m3, 0);
+    analogWrite(m4, 0);
+    cx_cord = P[i][1];
+    m = 0;
+    i++;
+  }
+}
+
